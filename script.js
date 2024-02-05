62842c5bede15e2d0667c9a4aad39ce1cad9f3ad
Clara Oliveira <122572905+mclaraoliveira@users.noreply.github.com>
2024-02-05 11:33:07 -0300

    Site mudado pro inglês porque o conteúdo da API só funciona nessa língua

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
+
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
@@ -24,14 +25,14 @@ function getMealList() {
                         </div>
                         <div class="meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href="#" class="recipe-btn">Ver Receita</a>
+                            <a href="#" class="recipe-btn">See Recipe</a>
                         </div>
                     </div>
                     `;
             });
             mealList.classList.remove('notFound');
         } else {
-            html = "Desculpe, não encontramos nenhuma receita!";
+            html = "Sorry, we were unable to find a recipe!";
             mealList.classList.add('notFound');
         }
 
@@ -55,14 +56,14 @@ function mealRecipeModal(meal){
     <h2 class="recipe-title"> ${meal.strMeal}</h2>
                 <p class="recipe-category">${meal.strCategory}</p>
                 <div class="recipe-instruction">
-                    <h3>Instruções:</h3>
+                    <h3>Instructions:</h3>
                     <p>${meal.strInstructions}</p>
                 </div>
                 <div class="recipe-meal-img">
                     <img src="${meal.strMealThumb}" alt="">
                 </div>
                 <div class="recipe-link">
-                    <a href="${meal.strYoutube}" target="_blank">Assista ao Vídeo</a>
+                    <a href="${meal.strYoutube}" target="_blank">Watch video</a>
                 </div>
                 `;
 
